8b7c6b6bca6a2b3fbdeeb46c9e806e31dce2f75c
Rashin <80585245+RashinShan@users.noreply.github.com>
2023-08-07 20:05:30 +0530

    Add files via upload

---
+++ Djangochat/chat/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static  
+
+urlpatterns = [
+
+    path('',views.home,name='home' ),
+     path('aboutus',views.aboutus,name='aboutus' ),
+    path('<str:room>/',views.room,name='room'),
+    path('checkview',views.checkview,name='checkview' ),
+    path('send',views.send,name='send' ),
+    path('getMessages/<str:room>/',views.getMessages,name='getMessages'),
+
+]
+
